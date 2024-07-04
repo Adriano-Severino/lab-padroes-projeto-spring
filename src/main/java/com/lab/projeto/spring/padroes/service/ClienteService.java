ad4c9551dd3fcdc2253289fad9043704eb171800
Adriano-Severino <adriano.x1@hotmail.com>
2024-07-04 10:42:21 -0300

    first commit

---
+++ src/main/java/com/lab/projeto/spring/padroes/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.lab.projeto.spring.padroes.service;
+
+import com.lab.projeto.spring.padroes.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
